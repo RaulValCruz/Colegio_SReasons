af85b21a293955e1207e924e9db4357b77be6662
RAVC <rravc00@hotmail.com>
2020-09-12 09:17:24 -0500

    Creacion de servicios

---
+++ Colegio/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Colegio.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Colegio.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UserController : ControllerBase
+  {
+    [HttpGet]
+    public async Task<ActionResult<List<Alumno>>> Get()
+    {
+      var listAlumnos = await GetListAlumnos();
+      if (listAlumnos.Count<0){
+        return NotFound(); 
+      }
+      return listAlumnos;
+    }
+
+    private async Task<List<Alumno>> GetListAlumnos()
+    {
+      var listAlumnos = new List<Alumno>()
+      {
+        new Alumno(){Id = 1, Nombre="Raúl Valdivia", Direccion="Coop VAB i3", Password="1234", FNacimiento="1991", Telefono="992233554"},
+        new Alumno(){Id = 2, Nombre="Arturo Cruz", Direccion="Coop VAB i3", Password="5678", FNacimiento="1991", Telefono="923557053"},
+      };
+      return listAlumnos;
+    }
+  }
+}
---
+++ Colegio/Models/Alumno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colegio.Models
+{
+  public class Alumno
+  {
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(120)")]
+    public string Nombre { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(18)")]
+    public string FNacimiento { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(16)")]
+    public string Password { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(180)")]
+    public string Direccion { get; set; }
+
+    [Required]
+    [Column(TypeName = "varchar(12)")]
+    public string Telefono { get; set; }
+  }
+}
